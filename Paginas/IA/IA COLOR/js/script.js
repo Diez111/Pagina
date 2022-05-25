d751a5afa9df1efc60cdf9d7f42d2e9d79fabb3d
Diez111 <71948453+Diez111@users.noreply.github.com>
2022-05-25 04:19:13 -0300

    first commit

---
+++ Paginas/IA/IA COLOR/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+        default_offset_pct: 0.7
+    });
+    $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({
+        default_offset_pct: 0.3,
+        orientation: 'vertical'
+    });
+});
